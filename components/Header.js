7dd0590855a13c683a0fa7d67dcc77135cd32dd8
kai-zong <zong.k@northeastern.edu>
2024-07-10 02:19:39 -0700

    feat: Add Start screen and Header component
    The code changes include adding a new screen called "Start" and a new component called "Header". The "Start" screen is imported in the main App component, replacing the previous text with the "Start" component. The "Header" component is a simple text component that displays a name.

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import {Text} from 'react-native';
+
+function Header({name}) {
+
+    return(
+        <Text>{name}</Text>
+    )
+}
+
+export default Header;
---
+++ screens/Start.js
@@ -0,0 +1,14 @@
+import {Button, Modal, TextInput, View} from 'react-native';
+import Header from '../components/Header';
+
+function Start() {
+  return (
+    <Modal>
+        <View>
+        <Header name={"Welcome"}/>
+        </View>
+    </Modal>
+  )
+}
+
+export default Start;
